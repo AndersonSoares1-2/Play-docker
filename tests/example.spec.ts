94246ecf4e77e24e775768bf0b4f0f0084ffe1d9
Anderson Soares <anderson.soares@tistech.co.ao>
2026-07-25 10:14:35 +0100

    via comando

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,17 @@
 import { test, expect } from "@playwright/test";
+import { config } from "dotenv";
+
+config();
 
 test("has title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  console.log("Environment variable TEST_ENV:", process.env.TEST_ENV);
+  console.log("Environment variable BASE_URL:", process.env.BASE_URL);
+  console.log("Environment variable USERNAME:", process.env.USERNAME);
+  console.log("Environment variable PASSWORD:", process.env.PASSWORD);
 });
 
 test("get started link", async ({ page }) => {
