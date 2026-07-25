005aceab27f984ce015ca5be03dcc643d987b2e9
Anderson Soares <andersonsoaresss123@gmail.com>
2026-07-25 10:59:46 +0100

    Update dotenv configuration to specify path and override option

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { config } from "dotenv";
 
-config();
+config({ path: "./.env", override: false });
 
 test("has title", async ({ page }) => {
   await page.goto("https://playwright.dev/");
